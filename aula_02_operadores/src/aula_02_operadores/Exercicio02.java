4bd071ec06aa08e6cd629c72d3a315c907ecf433
JoelRamalhoFilho <joelramalho.negociosonline@gmail.com>
2026-06-12 11:43:39 -0300

    Exercicio 02 resolvido

--- aula_02_operadores/src/aula_02_operadores/Exercicio02.java
+++ aula_02_operadores/src/aula_02_operadores/Exercicio02.java
@@ -1,11 +1,43 @@
 package aula_02_operadores;
 
+import java.util.Scanner;
+
 public class Exercicio02 {
 
 	public static void main(String[] args) {
 		
+		Scanner leia = new Scanner(System.in);
+		
+		float nota1;
+		float nota2;
+		float nota3;
+		float nota4;
+		float media;
 		
 		
+        System.out.print("\nDigite a Primeira Nota: ");
+		
+		nota1 = leia.nextFloat();
+		
+         System.out.print("\nDigite a Segunda Nota: ");
+			
+			nota2 = leia.nextFloat();
+			
+        System.out.print("\nDigite a Terceira Nota: ");
+			
+			nota3 = leia.nextFloat();
+		
+		System.out.print("\nDigite a Quarta Nota: ");
+				
+				nota4 = leia.nextFloat();
+				
+		media = ((nota1 + nota2 + nota3 + nota4) / 4);
+		
+		System.out.printf("A Média das notas é: %.2f", media);
+		
+		leia.close();
+				
+				
 	}
 
 }
