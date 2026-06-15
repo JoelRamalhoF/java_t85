7115ac62c2ab8bf98606b6d8677829fdfb49ecd7
JoelRamalhoFilho <joelramalho.negociosonline@gmail.com>
2026-06-15 12:47:00 -0300

    Exercicio 02 feito

---
+++ aula_03_condicionais/src/aula_03_condicionais/Exercicio02.java
@@ -0,0 +1,33 @@
+package aula_03_condicionais;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		int numero;
+		
+		System.out.println("Digite um número inteiro: ");
+		numero = leia.nextInt();
+	
+	
+	 if (numero % 2 == 0 && numero > 0 ) {
+		 System.out.println("O número é par e positivo");
+	 } else if(numero % 2 == 0 && numero < 0 ){
+		 System.out.println("O número é par e negativo");
+		 
+	 }else if(numero % 2 != 0 && numero < 0 ) {
+		 System.out.println("O número é ímpar e negativo");
+	 }else if(numero % 2 != 0 && numero > 0 ) {
+		 System.out.println("O número é ímpar e positivo");
+	 }else {
+		 System.out.println("O numero 0 é par e é um número inteiro");
+	 }
+		 
+		 leia.close();
+	}
+}
+
+
