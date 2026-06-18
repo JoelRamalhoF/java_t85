6f9ef77a03e392303f352d69898651e3668e1c5f
JoelRamalhoFilho <joelramalho.negociosonline@gmail.com>
2026-06-18 14:48:48 -0300

    Como o exercico era parecido, aproveitei a logica e editei para colocar o Set Integer

--- Aula06Collection/src/Ex04Collectionset.java
+++ Aula06Collection/src/Ex04Collectionset.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Ex04Collectionset {
 	
@@ -7,31 +9,32 @@ public class Ex04Collectionset {
 			
 			Scanner leia = new Scanner(System.in);
 			
-			// Vetor inicializado com dados
-			int vetorInteiros[] = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
-			int numero;
-			boolean encontrado = false; /* decidi criar um boolean pois tentei com if e else e tava dando muitos erros na hora de exibir a mensagem 
-			                                 de numero nao foi encontrado*/
+			//set integer, pegando todos numeros do exercicio e armazenando
+			Set<Integer> numeros = new HashSet<Integer>();
+			numeros.add(2);
+			numeros.add(5);
+			numeros.add(1);
+			numeros.add(3);
+			numeros.add(4);
+			numeros.add(9);
+			numeros.add(7);
+			numeros.add(8);
+			numeros.add(10);
+			numeros.add(6);
 
-			
+			int numero;
 	 
-			
+			//mensagem para o usuario digitar um numero inteiro e guardar na memoria
 			System.out.println("Digite um Número Inteiro que deseja encontrar: ");
-			numero = leia.nextInt();  // le o numero digitado e armazena na memoria
+			numero = leia.nextInt(); 
 			
-			//criado um laco for para percorrer as posicoes do vetor
-			for (int posicao = 0; posicao < vetorInteiros.length; posicao++) {
-				
-				/* se o usuario digitar o numero que se encaixa em uma posicao, a mensagem ira exibir o numero digitado 
-				 e a posicao referente a aquele numero */
-				
-				if (vetorInteiros[posicao] == numero) {
-				System.out.printf("O número %d esta na posição = %d\n", numero, posicao);
-				encontrado = true; //se atender a condicao de numero encontrado, o valor boolean muda para true e nao exibe a mensagem de numero nao encontrado
+			// condicao, se encontrar exibe numero encontrado, senao numero nao encontrado
+			
+			if (numeros.contains(numero)) {
+			    System.out.printf("O número %d foi encontrado!%n", numero);
+			} else {
+			    System.out.printf("O número %d não foi encontrado!%n", numero);
 			}
-				}
-			if (encontrado == false) //caso o numero nao seja encontrado na condicao acima, ele entende que boolean continua false e exibe a mensagem
-				System.out.printf("O número %d NÃO FOI ENCONTRADO", numero);
 			
 			leia.close();
 		
